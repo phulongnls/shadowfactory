b6542b0e1eb698922aa11f193ffc2eb0bdb35782
Hoang <nhathoang10217@gmail.com>
2019-03-19 16:35:05 +0700

    remove cache and logs folder

--- cache/compiled/files/934ef696a70b5405b970cd11ce733765.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/html/shadowfactory/user/config/streams.yaml',
-    'modified' => 1552886147,
-    'data' => [
-        
-    ]
-];
