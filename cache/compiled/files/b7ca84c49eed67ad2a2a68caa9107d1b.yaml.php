61f77bf5adb6e095cb3d50de17508baf567ae0ca
quocchi2210 <quocchi2210@gmail.com>
2019-03-19 16:32:08 +0700

    fix conflict

--- cache/compiled/files/b7ca84c49eed67ad2a2a68caa9107d1b.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/shadowfactory/user/config/media.yaml',
-    'modified' => 1552374200,
-    'data' => [
-        
-    ]
-];
